b517e6fa5538621b317993387f4ca8afca47749c
Jasa Cloud <account@jasacloud.com>
2018-03-21 14:03:40 +0700

    add

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -1,4 +1,7 @@
 <?PHP
+namespace Logger;
+use \Api\System as System;
+use \Api\Api as Api;
 /**
 	new Logger($_SERVER['DOCUMENT_ROOT']."/log/LOGGED.log", $_SERVER['PHP_SELF'].":".__LINE__."  ".$data);
 	new Logger("default", $_SERVER['PHP_SELF'].":".__LINE__."  ".$data);
